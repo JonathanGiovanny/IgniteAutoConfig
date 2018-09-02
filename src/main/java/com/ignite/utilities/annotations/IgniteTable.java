a4ac82b80bbd2d23ba1fa7d3524519ebf0659a79
JJOCAM <jongiocam@gmail.com>
2018-09-02 01:52:35 -0500

    [02/09/2018] Added notation and main classes

---
+++ src/main/java/com/ignite/utilities/annotations/IgniteTable.java
@@ -0,0 +1,15 @@
+package com.ignite.utilities.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface IgniteTable {
+
+	public String name() default "";
+	
+	public String cacheName();
+}
